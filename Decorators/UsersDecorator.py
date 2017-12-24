e1676a031c3549275187c6f7e6d674f309c3bb73
annamat <anna.matveev@gmail.com>
2017-12-25 01:42:33 +0200

    Fixing a but with indentations

--- Decorators/UsersDecorator.py
+++ Decorators/UsersDecorator.py
@@ -10,6 +10,7 @@ class UsersDecorator:
     def amend_user_details(reddit_user):
         comments_counter = 0
         posts_counter = 0
+
         try:
             user_comments = reddit_user.comments.new(limit=PrawConfig.USERS_NEW_COMMENTS_LIMIT)
         except prawcore.exceptions.NotFound:
@@ -20,11 +21,11 @@ class UsersDecorator:
                     and reddit_comment.subreddit.display_name == PrawConfig.SUBREDDIT:
                 comments_counter += 1
 
-            user_posts = reddit_user.submissions.new(limit=PrawConfig.USERS_NEW_COMMENTS_LIMIT)
-            for reddit_post in user_posts:
-                if reddit_post.subreddit.display_name == PrawConfig.SUBREDDIT:
-                    posts_counter += 1
+        user_posts = reddit_user.submissions.new(limit=PrawConfig.USERS_NEW_COMMENTS_LIMIT)
+        for reddit_post in user_posts:
+            if reddit_post.subreddit.display_name == PrawConfig.SUBREDDIT:
+                posts_counter += 1
 
-            user = User(reddit_user.id, reddit_user.name, reddit_user.link_karma, comments_counter, posts_counter)
+        user = User(reddit_user.id, reddit_user.name, reddit_user.link_karma, comments_counter, posts_counter)
 
         return user
